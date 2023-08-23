2d74031bc3e6bbd2ef635697ae333c1d65cf3d26
Andriikryl <andreyk557@gmail.com>
2023-08-23 18:43:59 +0300

    add new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import TicTacToe from "./components/TicTacToe/TicTacToe";
-
+import style from "./app.module.css";
+import { TextEncrypted } from "./components/TextEncrypted/TextEncrypted";
 function App() {
   return (
-    <main>
-      <TicTacToe />
+    <main className={style.main}>
+      <div className={style.container}>
+        <h1 className={style.game__title}>
+          <TextEncrypted text="Tic Tac Toe Game" interval={80} />
+        </h1>
+        <TicTacToe />
+      </div>
     </main>
   );
 }

fff8d49bd93237ec771fa29f051225abd87aae95
Andriikryl <andreyk557@gmail.com>
2023-08-23 17:37:04 +0300

    add new styles button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -7,11 +7,7 @@ interface IButton {
 export default function Button({ text, onClick }: IButton) {
   return (
     <button onClick={onClick} className={style.button}>
-      <span className={style.spark__container}>
-        <span className={style.spark} />
-      </span>
-      <span className={style.backdrop} />
-      <span className={style.text}>{text}</span>
+      <span>{text}</span>
     </button>
   );
 }
